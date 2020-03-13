6455fa9e76f93ac1c84ecdf80c7d09814eee9571
Shawn Williams <shawnwilliams@shawns-mbp.lan>
2020-03-12 21:39:53 -0400

    fixed objects inherited by function,

--- R/reactome_seq.R
+++ R/reactome_seq.R
@@ -2,7 +2,7 @@
 
 reactome_seq <- function(results.table, seperate.up.down = TRUE, p.cutoff = 0.05, lfc.cutoff = 0.7 ,
                          lfc.label = 'log2FoldChange', gene.label = 'Gene', gene.label.type = "FLYBASE",
-                         padj.label = 'padj', org = 'fly'){
+                         padj.label = 'padj', organism.type = 'fly'){
 
   lfc.label <- as.symbol(lfc.label)
   gene.label <- as.symbol(gene.label)
@@ -13,19 +13,19 @@ reactome_seq <- function(results.table, seperate.up.down = TRUE, p.cutoff = 0.05
     print('separating by up vs down fold change')
 
     gene.vector <- de.vector(results.table,seperate.up.down = 'up', p.cutoff , lfc.cutoff ,
-                             lfc.label, gene.label, gene.label.type, padj.label, org)
+                             lfc.label, gene.label, gene.label.type, padj.label, org = organism.type)
 
-    pwf.model <- pwf (gene.vector,org = org)
-    reactome.results.up <- reactome_goseq(gene.vector, pwf = pwf.model, org)
+    pwf.model <- pwf (gene.vector,org = organism.type)
+    reactome.results.up <- reactome_goseq(gene.vector, pwf = pwf.model, org = organism.type)
     #print(reactome.results.up)
 
 
     # reactome for down genes
     gene.vector <- de.vector(results.table,seperate.up.down = 'down', p.cutoff , lfc.cutoff ,
-                             lfc.label, gene.label, gene.label.type, padj.label, org)
+                             lfc.label, gene.label, gene.label.type, padj.label, org = organism.type)
 
-    pwf.model <- pwf (gene.vector,org = org)
-    reactome.results.down <- reactome_goseq(gene.vector, pwf = pwf.model, org )
+    pwf.model <- pwf (gene.vector,org = organism.type)
+    reactome.results.down <- reactome_goseq(gene.vector, pwf = pwf.model, org = organism.type )
 
     #print(reactome.results.down)
 
@@ -38,10 +38,10 @@ reactome_seq <- function(results.table, seperate.up.down = TRUE, p.cutoff = 0.05
   }
   else {
 
-    gene.vector <- de.vector(results.in,seperate.up.down = 'none')
+    gene.vector <- de.vector(results.in,seperate.up.down = 'none',  org = organism.type)
 
     pwf.model <- pwf (gene.vector,org = org)
-    reactome.results <- reactome_goseq(gene.vector, pwf = pwf.model, org)
+    reactome.results <- reactome_goseq(gene.vector, pwf = pwf.model, org = organism.type)
 
   }
 
