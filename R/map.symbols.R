bfddaa6164d659608934617145c8571a1d91fea7
Shawn Williams <shawnwilliams@shawns-mbp.lan>
2020-03-17 11:42:58 -0400

    formating

--- R/map.symbols.R
+++ R/map.symbols.R
@@ -2,8 +2,8 @@ map.symbols<- function(genes, .db, gene.label.type){
   ##### set up dbi parameters based on organism  #####
   
   
-  genes <- mapIds(.db, keys = genes,
-                                 column = "SYMBOL", keytype = gene.label.type, multiVals = 'first')  %>%
+  genes <- suppressMessages(mapIds(.db, keys = genes,
+                                 column = "SYMBOL", keytype = gene.label.type, multiVals = 'first'))  %>%
     as.character()
   # remove nas
   genes<- genes[!is.na(genes)]
