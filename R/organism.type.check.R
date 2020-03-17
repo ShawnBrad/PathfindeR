d3036a01cf833d69bf2c57e79ef585c823fc0ec1
Shawn Williams <shawnwilliams@shawns-mbp.lan>
2020-03-17 11:43:09 -0400

    formating

--- R/organism.type.check.R
+++ R/organism.type.check.R
@@ -1,3 +1,3 @@
 organism.type.check <- function(organism.type){
-  if (!(organism.type %in% valid.organisms)) stop(paste( organism.type ,' is not currently supported'))
+  if (!(organism.type %in% c('human','fly'))) stop(paste( organism.type ,' is not currently supported'))
 }
