f1e8360a5730d0c51e74500e5d6daed0a1d9502b
Shawn Williams <shawnwilliams@shawns-mbp.lan>
2020-03-18 11:16:15 -0400

    added roxygen comments

--- R/reactome_lolliplot.R
+++ R/reactome_lolliplot.R
@@ -1,4 +1,22 @@
 #' @export
+#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate
+#' @importFrom dplyr if_else
+#' @importFrom dplyr desc
+#' @importFrom dplyr arrange
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 xlab
+#' @importFrom ggplot2 ylab
+#' @importFrom ggplot2 coord_flip
+#' @importFrom ggplot2 scale_color_manual
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 scale_size
+#' @importFrom ggplot2 geom_segment
+#' @importFrom forcats fct_reorder
+#' @importFrom base return
+#' @importFrom base log10
+
 reactome_lolliplot <- function(res.reactome){
   colp <- c(up = 'darkred', down = 'darkblue')
   res.reactome %>%
